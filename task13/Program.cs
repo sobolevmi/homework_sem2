8e0d891b225244ea3152f070977e04713a2755d7
Maksim Sobolev <sobmig@gmail.com>
2022-06-28 00:14:29 +0300

    закончили написание кода программы (задача 13)

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void thirdNumber (int number)
+{
+ if (number / 100 > 1)
+    {
+        if (number > 100 & number < 1000)
+        {
+            Console.WriteLine (number + " -> " + number % 10);
+        }
+        else
+        {
+        Console.Write (number + " -> ");
+            while (number > 1000)
+            {
+            number = number / 10;
+            }
+        Console.WriteLine (number % 10);
+        }
+    }
+    else
+    {
+        Console.WriteLine (number + " -> " + "третьей цифры нет");
+    }
+}
+   
+Console.WriteLine ("Введите число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+thirdNumber (n);
