318c0f7673ae0f454aa642364100efdd1c4600b0
Maksim Sobolev <sobmig@gmail.com>
2022-06-28 00:38:58 +0300

    закончили написание кода программы (задача 15)

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void dayOfTheWeek (int number)
+{
+    if (number >= 1 & number < 6)
+    {
+        Console.WriteLine (number + " -> нет");
+    }
+    else
+    {
+    if (number >= 6 & number < 8)
+    {
+        Console.WriteLine (number + " -> да");
+    }
+    else
+        Console.WriteLine ("Неправильно введенное число");
+    }
+}
+
+Console.WriteLine ("Чтобы узнать, является ли день недели выходным, введите его порядковый номер (от 1 до 7): ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+dayOfTheWeek (n);
