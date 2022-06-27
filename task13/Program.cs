12ce68e5d64a1e9220b8552afd9ab47512ef5ada
Maksim Sobolev <sobmig@gmail.com>
2022-06-28 00:20:49 +0300

    убрали излишне усложняющие код строки

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,21 +1,14 @@
 ﻿void thirdNumber (int number)
 {
  if (number / 100 > 1)
-    {
-        if (number > 100 & number < 1000)
-        {
-            Console.WriteLine (number + " -> " + number % 10);
-        }
-        else
         {
         Console.Write (number + " -> ");
-            while (number > 1000)
+            while (number > 100 & number > 1000)
             {
             number = number / 10;
             }
         Console.WriteLine (number % 10);
         }
-    }
     else
     {
         Console.WriteLine (number + " -> " + "третьей цифры нет");
